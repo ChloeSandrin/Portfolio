121a55c63dbc87d885ebddcc34655a47e4cb4f83
Chloé Sandrin <csandrin1904@gmail.com>
2026-03-23 15:12:01 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,98 @@
+// =======================
+// TABLEAUX COMPÉTENCES
+// =======================
+
+const tableaux = document.querySelectorAll(
+  "#tableauboard, #illustratorTableau, #figmaTableau, #photoshopTableau, #vscodeTableau, #indesignTableau"
+);
+
+function cacherTousLesTableaux() {
+  tableaux.forEach(tableau => {
+    tableau.style.display = "none";
+  });
+}
+
+cacherTousLesTableaux();
+document.getElementById("tableauboard").style.display = "block";
+
+const correspondance = {
+  illustrator: "illustratorTableau",
+  figma: "figmaTableau",
+  photoshop: "photoshopTableau",
+  vscode: "vscodeTableau",
+  indesign: "indesignTableau"
+};
+
+Object.keys(correspondance).forEach(idLogo => {
+  const logo = document.getElementById(idLogo);
+
+  if (logo) {
+    logo.addEventListener("click", () => {
+      cacherTousLesTableaux();
+      document.getElementById(correspondance[idLogo]).style.display = "flex";
+    });
+  }
+});
+
+
+// =======================
+// HOVER PROJETS
+// =======================
+
+const images = document.querySelectorAll(".projet-img");
+
+images.forEach(img => {
+  const originalSrc = img.src;
+  const hoverSrc = img.dataset.hover;
+
+  img.addEventListener("mouseenter", () => {
+    img.src = hoverSrc;
+  });
+
+  img.addEventListener("mouseleave", () => {
+    img.src = originalSrc;
+  });
+});
+
+
+// =======================
+// NAVBAR ACTIVE AVEC DELAI
+// =======================
+
+const sections = document.querySelectorAll("#P1, #P2, #P3, #P4");
+const navLinks = document.querySelectorAll("nav a");
+
+let timeoutId = null;
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+
+      if (entry.isIntersecting) {
+
+        const currentId = entry.target.id;
+
+        
+        clearTimeout(timeoutId);
+
+        timeoutId = setTimeout(() => {
+
+          navLinks.forEach(link => {
+            link.classList.remove("active");
+
+            if (link.getAttribute("href") === `#${currentId}`) {
+              link.classList.add("active");
+            }
+          });
+
+        }, 500); 
+      }
+
+    });
+  },
+  {
+    threshold: 0.6 
+  }
+);
+
+sections.forEach(section => observer.observe(section));
